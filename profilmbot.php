a1d9d621442203e2026da72444173523bddfc456
vsplesk92 <abbasov.roman.92@yandex.ru>
2016-04-25 15:40:05 +0300

    Update profilmbot.php

--- profilmbot.php
+++ profilmbot.php
@@ -1,13 +1,16 @@
 <?php
 
-ini_set('error_reporting', E_ALL);
-
 $botToken = "184934299:AAHZblySTWCGDiUeCoLOqoehp0FP0x-NeyE";
 $website = "https://api.telegram.org/bot".$botToken;
 
 $update = file_get_contents("php://input");
 $update = json_decode ($update, TRUE);
 
+/**
+ * Oсновные переменные.
+ */
+ 
+$output = json_decode(file_get_contents('php://input'), TRUE);
 $chatId = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
 
